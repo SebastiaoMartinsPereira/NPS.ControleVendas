4ab0b6aace004f7b805cb01d72181c9a2e9273c4
Sebastiao Martins Pereira <projetoti@outlook.com>
2023-07-28 19:05:00 -0300

    ajustes arquivos

--- NPS.ControleVendas/src/NPS.AuthApi/Data/MongoRepository.cs
+++ NPS.ControleVendas/src/NPS.AuthApi/Data/MongoRepository.cs
@@ -1,38 +1,10 @@
-﻿using Microsoft.AspNetCore.DataProtection.XmlEncryption;
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
 using MongoDB.Driver;
-using NPS.AuthApi.Domain;
 using NPS.AuthApi.Model;
 using System.Linq.Expressions;
 
 namespace NPS.AuthApi.Data
 {
-    public interface IMongoRepository<TDocument> where TDocument : IDocument
-    {
-        IQueryable<TDocument> AsQueryable();
-        IEnumerable<TDocument> FilterBy(Expression<Func<TDocument, bool>> filterExpression);
-        IEnumerable<TProjected> FilterBy<TProjected>(
-            Expression<Func<TDocument, bool>> filterExpression,
-            Expression<Func<TDocument, TProjected>> projectionExpression);
-        TDocument FindOne(Expression<Func<TDocument, bool>> filterExpression);
-        Task<TDocument> FindOneAsync(Expression<Func<TDocument, bool>> filterExpression);
-        TDocument FindById(string id);
-        Task<TDocument> FindByIdAsync(string id);
-        void InsertOne(TDocument document);
-        Task InsertOneAsync(TDocument document);
-        void InsertMany(ICollection<TDocument> documents);
-        Task InsertManyAsync(ICollection<TDocument> documents);
-        void ReplaceOne(TDocument document);
-        Task ReplaceOneAsync(TDocument document);
-        void DeleteOne(Expression<Func<TDocument, bool>> filterExpression);
-        Task DeleteOneAsync(Expression<Func<TDocument, bool>> filterExpression);
-        void DeleteById(string id);
-        Task DeleteByIdAsync(string id);
-        void DeleteMany(Expression<Func<TDocument, bool>> filterExpression);
-        Task DeleteManyAsync(Expression<Func<TDocument, bool>> filterExpression);
-    }
-
-
     public class MongoRepository<TDocument> : IMongoRepository<TDocument> where TDocument : IDocument
     {
         private readonly IMongoCollection<TDocument> _collection;
